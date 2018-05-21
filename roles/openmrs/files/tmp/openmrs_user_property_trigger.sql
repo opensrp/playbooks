ca7ec322d2992cdfe353b7b1370d6a910d74330a
manutarus <manutarus@gmail.com>
2018-05-21 10:24:38 +0300

    Added a temp table as a place holder for creating user trigger

--- roles/openmrs/files/tmp/openmrs_user_property_trigger.sql
+++ roles/openmrs/files/tmp/openmrs_user_property_trigger.sql
@@ -1,5 +1,15 @@
 -- Sql to create a trigger in mysql OpenMRS database to prevent admin and other user accounts from locking
 -- By resetting the property_value for login attempts to zero every its increased.
+
+-- Create a temp user_property table to hold the trigger
+CREATE TABLE IF NOT EXISTS user_property (
+  user_id int(11) NOT NULL DEFAULT '0',
+  property varchar(100) NOT NULL DEFAULT '',
+  property_value varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (user_id,property)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Create the trigger
 DELIMITER $$
 CREATE TRIGGER user_property_before_update BEFORE UPDATE ON user_property
   FOR EACH ROW
