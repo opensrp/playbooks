0ca9bc75c236a61eed85a405f676bc51fa08d0de
Ephraim Kigamba <ekigamba@ona.io>
2018-06-26 12:01:49 +0300

    Remove couchdb pid scripts

--- roles/monit/files/create_couchdb_pid.sh
+++
@@ -1,5 +0,0 @@
-COUCHDB_PID="$(ps aux | grep /var/lib/couchdb | grep -v grep | awk '{print $2}')"
-
-cat > /var/run/couchdb.pid << EOF
-$COUCHDB_PID
-EOF
--- roles/monit/files/get_couchdb_pid.sh
+++
@@ -1,1 +0,0 @@
-echo "$(ps aux | grep /var/lib/couchdb | grep -v grep | awk '{print $2}')"
