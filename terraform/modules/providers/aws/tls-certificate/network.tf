512a0cdf3f933a07ea4621e80827aa172cbb5921
Ukang'a Dickson <ukanga@users.noreply.github.com>
2020-08-07 11:39:59 +0300

    Terraform updates (#235)
    * Transform aws_acm_certificate.main.domain_validation_options to a list
    
    The variable aws_acm_certificate.main.domain_validation_options contains
    a set and as such I chose to transform it into a list for access to the
    variables.
    
    * Add load-balancer module changes
    
    See https://github.com/onaio/terraform-aws-alb/pull/1

--- terraform/modules/providers/aws/tls-certificate/network.tf
+++ terraform/modules/providers/aws/tls-certificate/network.tf
@@ -20,8 +20,8 @@ resource "aws_acm_certificate" "main" {
 resource "aws_route53_record" "cert_validation" {
   count      = length(var.tls_certificate_domain_name_cnames) + 1
   depends_on = [aws_acm_certificate.main]
-  name       = aws_acm_certificate.main.domain_validation_options[count.index]["resource_record_name"]
-  type       = aws_acm_certificate.main.domain_validation_options[count.index]["resource_record_type"]
+  name       = tolist(aws_acm_certificate.main.domain_validation_options)[count.index]["resource_record_name"]
+  type       = tolist(aws_acm_certificate.main.domain_validation_options)[count.index]["resource_record_type"]
   zone_id    = data.aws_route53_zone.main.id
   # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
   # force an interpolation expression to be interpreted as a list by wrapping it
@@ -31,7 +31,7 @@ resource "aws_route53_record" "cert_validation" {
   # If the expression in the following list itself returns a list, remove the
   # brackets to avoid interpretation as a list of lists. If the expression
   # returns a single list item then leave it as-is and remove this TODO comment.
-  records = [aws_acm_certificate.main.domain_validation_options[count.index]["resource_record_value"]]
+  records = [tolist(aws_acm_certificate.main.domain_validation_options)[count.index]["resource_record_value"]]
   ttl     = 60
 }
 
